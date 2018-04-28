54e553eea0edc5ff97a22f57dc25b6919d26b05a
shirakaba <14055146+shirakaba@users.noreply.github.com>
2018-04-28 21:54:43 +0100

    token-tests.tsx complete

--- src/ui/token-tests.tsx
+++ src/ui/token-tests.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { ITokenProps, Token } from './token';
+import { ReactElement } from 'react';
 
 const tokenProps: ITokenProps = {
     x: 0,
@@ -14,3 +15,26 @@ const tokenProps: ITokenProps = {
     children: [],
     animationDuration: 0,
 };
+
+const tsxTests: ReactElement<ITokenProps>[] = [
+    (
+        <Token x={1} y={2} animate={true}>
+            <p>foo</p>
+        </Token>
+    ),
+    (
+        <Token x={0} y={0} animate={false} onClick={tokenProps.onClick}>
+            <p>foo</p>
+        </Token>
+    ),
+    (
+        <Token x={0} y={0} animate={false} onMouseOver={tokenProps.onMouseOver}>
+            <p>foo</p>
+        </Token>
+    ),
+    (
+        <Token x={0} y={0} animate={false} onMouseOut={tokenProps.onMouseOut}>
+            <p>foo</p>
+        </Token>
+    )
+];
