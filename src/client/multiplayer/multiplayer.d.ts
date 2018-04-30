1c7c31321d154f44450db9d36122ea8f1f0d32d3
shirakaba <14055146+shirakaba@users.noreply.github.com>
2018-05-01 00:58:08 +0100

    multiplayer.d.ts completed (having installed socket.io-client typings)

--- src/client/multiplayer/multiplayer.d.ts
+++ src/client/multiplayer/multiplayer.d.ts
@@ -1,27 +1,37 @@
-// import { RestoreAction } from '../../core/action-creators';
 import { ActionTypes } from '../../core/action-types';
-import { Action, ActionSignatures } from '../../core/action-creators';
+import { Store } from 'redux';
+// import { Action, ActionSignatures } from '../../core/action-creators';
 
 declare const blacklistedActions: Set<ActionTypes>;
 export interface MultiplayerInput {
-    socket: Socket,
-    socketOpts: any,
+    socket: SocketIOClient.Socket|MockSocket,
+    socketOpts?: SocketIOClient.ConnectOpts,
     gameID: string,
     playerID: string|null,
     gameName: string,
     numPlayers: number,
-    server: any,
+    server: string,
 }
+export type SocketEvents = "action"|"sync"|"connect"|"disconnect";
 export type SocketCallback = (arg0: any, arg1: any) => void;
-export interface Socket {
+export interface MockSocket {
     callbacks: { [type: string]: SocketCallback; }
-    // FIXME: Can't figure this one out, looking at the tests. Here's my best guess:
-    // emit: (actionType: ActionTypes, action: ActionSignatures, stateID: number, gameID: string, playerID: string|null) => Action;
-    emit: (type: string, ...args: any[]) => Action;
+    // Can't figure this one out, looking at the tests - but it's only a mock, anyway. Here's my best guess:
+    // emit: (actionType: ActionTypes, action: ActionSignatures, stateID: number, gameID: string, playerID: string|null) => any;
+    emit(type: string, ...args: any[]): any;
     receive(type: string, ...args: any[]): void;
-    on(type: string, callback: SocketCallback): void;
+    on(event: string, fn: Function): SocketIOClient.Emitter;
 }
 export class Multiplayer {
+    socket?: SocketIOClient.Socket|MockSocket;
+    readonly socketOpts?: SocketIOClient.ConnectOpts;
+    gameID: string;
+    playerID: string|null;
+    readonly gameName: string;
+    readonly numPlayers: number;
+    readonly server: string;
+    callback: () => any;
+    store: Store<any>|null;
     constructor({
         socket,
         socketOpts,
@@ -31,4 +41,9 @@ export class Multiplayer {
         numPlayers,
         server,
     }: MultiplayerInput);
+    createStore(reducer: any, enhancer?: any): Store<any>;
+    connect(): void;
+    subscribe(fn: () => any): void;
+    updateGameID(id: string): void;
+    updatePlayerID(id: string): void;
 }
