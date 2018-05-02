4289b34bf808534a6f4d4f6233acf5055489c354
shirakaba <14055146+shirakaba@users.noreply.github.com>
2018-05-02 20:43:05 +0100

    App.tsx created from App.js

--- examples/RNTS/App.tsx
+++ examples/RNTS/App.tsx
@@ -4,55 +4,39 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Image,
+    StyleSheet,
+    View,
 } from 'react-native';
+import { Client, WrappedBoard } from '../../src/client/react-native';
+// import logo from './assets/img/logo.png'; // Doesn't find it, somehow. Maybe a TypeScript issue (tsc ignores all but .ts(x) files).
+const logo = require('./assets/img/logo.png');
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const App: WrappedBoard = Client({
+    game: TicTacToe,
+    board: Board,
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+const Singleplayer = () => (
+    <View style={styles.container}>
+        <Image source={logo} style={styles.logo} />
+        <App gameID="single" />
+    </View>
+);
+export default Singleplayer;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        width: 300,
+        height: 90,
+        marginBottom: 24,
+    },
 });
