4bfae7853a5b67496ea96901c2736e4ee983e271
shirakaba <14055146+shirakaba@users.noreply.github.com>
2018-05-03 20:11:47 +0100

    client.d.ts done

---
+++ packages/client.d.ts
@@ -0,0 +1,9 @@
+import {
+    Client, createEventDispatchers, createMoveDispatchers, _ClientImpl,
+    Dispatchers, ClientState, ClientInput,
+} from '../src/client/client';
+
+export {
+    Client, // createEventDispatchers, createMoveDispatchers, _ClientImpl,
+    Dispatchers, ClientState, ClientInput,
+};
