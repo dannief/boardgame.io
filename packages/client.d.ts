7d5d4ae0c84f50097a59338b5321b98b990d21d3
shirakaba <14055146+shirakaba@users.noreply.github.com>
2018-05-03 20:31:18 +0100

    more typings for client package

--- packages/client.d.ts
+++ packages/client.d.ts
@@ -3,7 +3,35 @@ import {
     Dispatchers, ClientState, ClientInput,
 } from '../src/client/client';
 
+import {
+    // Debug, DebugMove, DebugMoveArgField, KeyboardShortcut,
+    DebugMoveState, DebugMoveArgFieldProps, DebugMoveArgFieldState, DebugMoveProps, DebugProps, DebugState, KeyboardShortcutProps, KeyboardShortcutState
+} from '../src/client/debug/debug';
+
+import {
+    // GameLog,
+    GameLogState, GameLogProps
+} from '../src/client/log/log';
+
+import {
+    // Multiplayer,
+    MultiplayerInput, MockSocket
+} from '../src/client/multiplayer/multiplayer';
+
 export {
+    /* client */
     Client, // createEventDispatchers, createMoveDispatchers, _ClientImpl,
     Dispatchers, ClientState, ClientInput,
+
+    /* debug */
+    // Debug, DebugMove, DebugMoveArgField, KeyboardShortcut,
+    DebugMoveState, DebugMoveArgFieldProps, DebugMoveArgFieldState, DebugMoveProps, DebugProps, DebugState, KeyboardShortcutProps, KeyboardShortcutState,
+
+    /* log */
+    // GameLog,
+    GameLogState, GameLogProps,
+
+    /* multiplayer */
+    // Multiplayer,
+    MultiplayerInput, MockSocket
 };
